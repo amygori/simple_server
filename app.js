9421ef370897a17d3db2d46285d517df49de4289
Amy Gori <amydgori@gmail.com>
2017-10-27 23:20:47 -0400

    clean up unnecessary stuff from boilerplate

--- app.js
+++ app.js
@@ -9,10 +9,8 @@ const index = require('./routes/index');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(logger('dev'));
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,7 +27,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 module.exports = app;
